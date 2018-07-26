79ab1292413e9e02b83a25ee3dcf56b5b6e6252a
Angela Andaleon <angie.andaleon@gmail.com>
2018-07-26 14:36:19 -0500

    Update 15_ancestry_pipeline.sh

--- 15_ancestry_pipeline.sh
+++ 15_ancestry_pipeline.sh
@@ -2,7 +2,8 @@
 
 #RUNNING THE SAMPLE TOY DATA
 cd /home/angela/px_his_chol/ancestry_pipeline/toy_data/
-for i in {22..22};
+for i in {1..22};
+#for i in {22..22};
 do 
   #1. Split into chromosomes
   plink --bfile ACB_example --chr ${i} --make-bed --geno 0.01 --out ACB_example_chr${i};
@@ -12,8 +13,10 @@ do
   shapeit --input-ref /home/angela/1000GP_Phase3_combined/1000GP_Phase3_chr${i}.hap.gz /home/angela/1000GP_Phase3_combined/1000GP_Phase3_chr${i}.legend.gz /home/angela/1000GP_Phase3_combined/1000GP_Phase3.sample -B ACB_example_chr${i} --duohmm --input-map /home/angela/1000GP_Phase3_combined/genetic_map_chr${i}_combined_b37.txt --exclude-snp ACB_example_chr${i}.mendel.snp.strand.exclude --output-max ACB_example_chr${i}.haps.gz ACB_example_chr${i}.sample
   #4. Convert to RFMix input
   python ../shapeit2rfmix.py --shapeit_hap_ref ACB_example_chr${i}.haps.gz --shapeit_hap_admixed ACB_example_chr${i}.haps.gz --shapeit_sample_ref ACB_example_chr${i}.sample --shapeit_sample_admixed ACB_example_chr${i}.sample --ref_keep ACB_example.ref --admixed_keep ACB_example.notref --chr ${i} --genetic_map /home/angela/1000GP_Phase3_combined/genetic_map_chr${i}_combined_b37.txt --out ACB_example
+  #at some point in here your combined the references?
   #5. Infer local ancestry
 done
+
 
 
 
