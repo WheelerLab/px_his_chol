04fd0f08eb18f11b2c2492d58f957d7c1e456318
Angela Andaleon <angie.andaleon@gmail.com>
2018-07-30 10:53:22 -0500

    Update 15_ancestry_pipeline.sh

--- 15_ancestry_pipeline.sh
+++ 15_ancestry_pipeline.sh
@@ -5,6 +5,7 @@ cd /home/angela/px_his_chol/ancestry_pipeline/toy_data/
 for i in {1..22};
 #for i in {22..22};
 do 
+  #Note: the original .bed/.bim/.fam already has combined reference/test
   #1. Split into chromosomes
   plink --bfile ACB_example --chr ${i} --make-bed --geno 0.01 --out ACB_example_chr${i};
   #2. Check data is aligned to reference
@@ -13,12 +14,18 @@ do
   shapeit --input-ref /home/angela/1000GP_Phase3_combined/1000GP_Phase3_chr${i}.hap.gz /home/angela/1000GP_Phase3_combined/1000GP_Phase3_chr${i}.legend.gz /home/angela/1000GP_Phase3_combined/1000GP_Phase3.sample -B ACB_example_chr${i} --duohmm --input-map /home/angela/1000GP_Phase3_combined/genetic_map_chr${i}_combined_b37.txt --exclude-snp ACB_example_chr${i}.mendel.snp.strand.exclude --output-max ACB_example_chr${i}.haps.gz ACB_example_chr${i}.sample
   #4. Convert to RFMix input
   python ../shapeit2rfmix.py --shapeit_hap_ref ACB_example_chr${i}.haps.gz --shapeit_hap_admixed ACB_example_chr${i}.haps.gz --shapeit_sample_ref ACB_example_chr${i}.sample --shapeit_sample_admixed ACB_example_chr${i}.sample --ref_keep ACB_example.ref --admixed_keep ACB_example.notref --chr ${i} --genetic_map /home/angela/1000GP_Phase3_combined/genetic_map_chr${i}_combined_b37.txt --out ACB_example
-  #at some point in here your combined the references?
-  #5. Infer local ancestry
 done
 
+#5. Fix classes file for RFMix
+python ../classes.py --ref CEU_example.keep,YRI_example.keep --sample CEU_YRI_ACB.sample --out CEU_YRI_ACB.classes
+  #MUST KEEP CONSISTENT WITH INDVIDUAL ORDER IN THE ALLELES FILE
 
-
+#6. Run RFMix
+cd /home/angela/px_his_chol/RFMix/RFMix_v1.5.4/
+for i in {1..22};
+do
+  python RunRFMix.py -e 2 -w 0.2 --num-threads 40 --use-reference-panels-in-EM --forward-backward PopPhased /home/angela/px_his_chol/ancestry_pipeline/toy_data/ACB_example_chr${i}.alleles /home/angela/px_his_chol/ancestry_pipeline/toy_data/CEU_YRI_ACB.classes /home/angela/px_his_chol/ancestry_pipeline/toy_data/ACB_example_chr${i}.snp_locations -o /home/angela/px_his_chol/ancestry_pipeline/toy_data/CEU_YRI_ACB_chr${i}.rfmix
+done
 
 
 
