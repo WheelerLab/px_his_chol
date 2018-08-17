17bbd0e65e0668dc73514e8652f63e639fc592b7
Angela Andaleon <angie.andaleon@gmail.com>
2018-08-17 10:55:01 -0500

    Update 15_ancestry_pipeline.sh

--- 15_ancestry_pipeline.sh
+++ 15_ancestry_pipeline.sh
@@ -19,6 +19,7 @@ do
   #2. Phase w/ HAPI-UR
   #rate-limiting step
   /home/angela/px_his_chol/HAPI-UR/hapi-ur-1.01/hapi-ur -p chr${i} -w 256 -o phase_chr${i}
+  #Chromosome 22 (23754 variants and 12477 people) took 34552.859 seconds in HAPI-UR
 
   #3. Make additional files for RFMix input
   awk '{print $3}' phase_chr${i}.phsnp > phase_chr${i}.snp_locations
@@ -30,7 +31,17 @@ do
   #4. Run RFMix
   #rate-limiting step
   cd /home/angela/px_his_chol/RFMix/RFMix_v1.5.4/
-  python RunRFMix.py -e 2 -w 0.2 --num-threads 40 --use-reference-panels-in-EM --forward-backward PopPhased /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/phase_chr${i}.phgeno /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/HCHS.classes /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/phase_chr${i}.snp_locations -o /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/phase_chr${i}.rfmix
+  python RunRFMix.py \
+  -e 2 \
+  -w 0.2 \
+  --num-threads 50 \
+  --use-reference-panels-in-EM \
+  --forward-backward \
+  PopPhased \
+  /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/phase_chr${i}.phgeno \
+  /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/HCHS.classes \
+  /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/phase_chr${i}.snp_locations \
+  -o /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/phase_chr${i}.rfmix
 done
 
 #will take many days, check up again when classes start
