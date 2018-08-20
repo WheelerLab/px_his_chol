37a0177566c2e8f67c3a78cc5e097fd5f1ed8b49
Angela Andaleon <angie.andaleon@gmail.com>
2018-08-20 16:34:57 -0500

    Update and rename 18_run_rG.sh to 18_split_RFMix input.py

--- 18_run_rG.sh
+++
@@ -1,1 +0,0 @@
-#Based on https://github.com/aandaleon/DivPop/blob/master/GCTA_exp/03_est_gen_corr.r
---
+++ 18_split_RFMix input.py
@@ -0,0 +1,1 @@
+#Split up an input of individuals into smaller proportions, all of which contain all reference samples and a subset of the test population
