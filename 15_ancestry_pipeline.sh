b8969d08da4b97cd0c47d0b33363b02380cee909
Angela Andaleon <angie.andaleon@gmail.com>
2018-08-23 10:03:49 -0500

    Update 15_ancestry_pipeline.sh

--- 15_ancestry_pipeline.sh
+++ 15_ancestry_pipeline.sh
@@ -1,50 +1,53 @@
-#!/bin/bash
-#PBS -N test_anc_pipeline_1000G_ref
-#PBS -S /bin/bash
-#PBS -l walltime=500:00:00
-#PBS -l nodes=1:ppn=2
-#PBS -l mem=200gb
-#PBS -o logs/${PBS_JOBNAME}.o${PBS_JOBID}.log
-#PBS -e logs/${PBS_JOBNAME}.e${PBS_JOBID}.err
-cd $PBS_O_WORKDIR
 #Personal version of https://github.com/armartin/ancestry_pipeline
-
+#Using MOSAIC instead of RFMix
 #RUN ALL CHRS
 for i in {1..22};
 do
+
+##NAT
   #1. Extract chr with PLINK
-  cd /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/
-  /usr/local/bin/plink --bfile 1000G_HCHS_geno_0.01_maf_0.05_ordered --chr ${i} --make-bed --out chr${i}
-
+  cd /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/sep_pops/
+  /usr/local/bin/plink --bfile 1000G_HCHS_geno_0.01_maf_0.05_ordered --keep NAT.txt --chr ${i} --make-bed --out NAT_chr${i}
   #2. Phase w/ HAPI-UR
-  #rate-limiting step
-  /home/angela/px_his_chol/HAPI-UR/hapi-ur-1.01/hapi-ur -p chr${i} -w 64 -o phase_chr${i}
-  #Having a larger window size causes an error (13:00, 8/20/18); attempting all chr. w/ -w 64 for now
-  #Chromosome 22 (23754 variants and 12477 people, w = 256) took 34552.859 seconds in HAPI-UR
-
-  #3. Make additional files for RFMix input
-  awk '{print $3}' phase_chr${i}.phsnp > phase_chr${i}.snp_locations
-  /usr/bin/Rscript /home/angela/px_his_chol/ancestry_pipeline/make_classes_from_HAPI-UR.R /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/phase_chr${i}.phind /home/angela/px_his_chol/ancestry_pipeline/HCHS/ordered_pops.txt HCHS
+  /home/angela/px_his_chol/HAPI-UR/hapi-ur-1.01/hapi-ur -p NAT_chr${i} -w 64 -o NAT_chr${i}
+  #3. Rename HAPI-UR output for MOSAIC input
+  mv NAT_chr${i}.phgeno NATgenofile.${i}
+  printf ":sites:" > rates.${i}
+  wc -l < NATgenofile.${i} >> rates.${i} #add number of sites
+  awk '{print $4}' NAT_chr${i}.phsnp | paste -sd ' ' >> rates.${i} #add row of positions
+  awk '{print $3}' NAT_chr${i}.phsnp | paste -sd ' ' >> rates.${i} #add row of cM
+  mv NAT_chr${i}.phsnp snpfile.${i}
+  
+##IBS
+  #4. Extract chr with PLINK
+  /usr/local/bin/plink --bfile 1000G_HCHS_geno_0.01_maf_0.05_ordered --keep IBS.txt --chr ${i} --make-bed --out IBS_chr${i}
+  #5. Phase w/ HAPI-UR
+  /home/angela/px_his_chol/HAPI-UR/hapi-ur-1.01/hapi-ur -p IBS_chr${i} -w 64 -o IBS_chr${i}
+  #6. Rename HAPI-UR output for MOSAIC input
+  mv IBS_chr${i}.phgeno IBSgenofile.${i}
+  
+##YRI
+  #7. Extract chr with PLINK
+  /usr/local/bin/plink --bfile 1000G_HCHS_geno_0.01_maf_0.05_ordered --keep YRI.txt --chr ${i} --make-bed --out YRI_chr${i}
+  #8. Phase w/ HAPI-UR
+  /home/angela/px_his_chol/HAPI-UR/hapi-ur-1.01/hapi-ur -p YRI_chr${i} -w 64 -o YRI_chr${i}
+  #9. Rename HAPI-UR output for MOSAIC input
+  mv YRI_chr${i}.phgeno YRIgenofile.${i}
+  
+##HCHS
+  #10. Extract chr with PLINK
+  /usr/local/bin/plink --bfile 1000G_HCHS_geno_0.01_maf_0.05_ordered --keep HCHS.txt --chr ${i} --make-bed --out HCHS_chr${i}
+  
+  #FOR TESTING PURPOSES KEEP SMALL SUBSET
+  #/usr/local/bin/plink --bfile HCHS_chr${i} --thin-indiv-count 100 --make-bed --out HCHS_chr${i}
+  
+  #11. Phase w/ HAPI-UR
+  /home/angela/px_his_chol/HAPI-UR/hapi-ur-1.01/hapi-ur -p HCHS_chr${i} -w 64 -o HCHS_chr${i}
+  #12. Rename HAPI-UR output for MOSAIC input
+  mv HCHS_chr${i}.phgeno HCHSgenofile.${i}
+  
+##Run MOSAIC
+  mkdir -p MOSAIC_RESULTS #why doesn't MOSAIC automatically make these
+  mkdir -p MOSAIC_PLOTS
+  /usr/bin/Rscript /home/angela/px_his_chol/MOSAIC/mosaic.R HCHS /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/sep_pops/ -a 3 -n 24000 -c ${i}:${i}
 done
-
-for i in {1..22};
-do
-  #4. Run RFMix
-  #rate-limiting step
-  #RUNNING INTO MEMORY ISSUES
-    #As of 8/20/18, writing pipeline to cut each input into say 100 HCHS ind. each
-  cd /home/angela/px_his_chol/RFMix/RFMix_v1.5.4/
-  python RunRFMix.py \
-  -e 2 \
-  -w 0.2 \
-  --num-threads 50 \
-  --use-reference-panels-in-EM \
-  --forward-backward \
-  PopPhased \
-  /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/phase_chr${i}.phgeno \
-  /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/HCHS.classes \
-  /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/phase_chr${i}.snp_locations \
-  -o /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/phase_chr${i}.rfmix
-done
-
-#will take many days, check up again when classes start
