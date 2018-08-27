2e2aab0fd4b3f7cc6151fe91489cf3da29d20255
Angela Andaleon <angie.andaleon@gmail.com>
2018-08-27 11:02:21 -0500

    Update 15_ancestry_pipeline.sh

--- 15_ancestry_pipeline.sh
+++ 15_ancestry_pipeline.sh
@@ -45,11 +45,11 @@ do
   /home/angela/px_his_chol/HAPI-UR/hapi-ur-1.01/hapi-ur -p HCHS_chr${i} -w 64 -o HCHS_chr${i}
   #12. Rename HAPI-UR output for MOSAIC input
   mv HCHS_chr${i}.phgeno HCHSgenofile.${i}
-  
+done
+
 ##Run MOSAIC
   mkdir -p MOSAIC_RESULTS #why doesn't MOSAIC automatically make these
   mkdir -p MOSAIC_PLOTS
-  /usr/bin/Rscript /home/angela/px_his_chol/MOSAIC/mosaic.R HCHS /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/sep_pops/ -a 3 -n 24000 -c ${i}:${i}
-done
+  /usr/bin/Rscript /home/angela/px_his_chol/MOSAIC/mosaic.R HCHS /home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/sep_pops/ -a 3 -n 24000 -c 1:22
 
 #how in the world do you read MOSAIC output
