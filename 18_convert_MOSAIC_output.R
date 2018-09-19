c8fa461c6735111bf649e41a091b5ad8bd61d5cb
Angela Andaleon <angie.andaleon@gmail.com>
2018-09-19 10:38:39 -0500

    Update 18_convert_MOSAIC_output.R

--- 18_convert_MOSAIC_output.R
+++ 18_convert_MOSAIC_output.R
@@ -1,16 +1,20 @@
 #MAKE THIS MORE READABLE
-
 #converts local ancestry output from MOSAIC to a more human-readable format
-load("/home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/sep_pops/100_ind/MOSAIC_RESULTS/localanc_HCHS_3way_1-100_22-22_684_60_0.99_100.RData")
+
 library(data.table)
 library(dplyr)
 library(MOSAIC)
 library(tibble)
 library(reshape2)
+args <- commandArgs(trailingOnly = T)
+input_file_name <- args[1] #localanc.RData produced by MOSAIC
+output_file_name <- args[2] #output prefix
+
+load(input_file_name)
 df <- reshape2::melt(localanc[[1]])
   #Var1 is each of the 3 possible ancestries
   #Var2 is haplotype
-  #Var3 is location
+  #Var3 is location (bp)
 g_loc <- tibble::rownames_to_column(as.data.frame(g.loc), "Var3")
 g_loc$Var3 <- as.integer(g_loc$Var3)
 loc_anc <- dplyr::left_join(df, g_loc)
@@ -27,8 +31,8 @@ snpfile <- fread("/home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmeri
 snpfile <- snpfile %>% dplyr::select(V3, V4)
 colnames(snpfile) <- c("cM", "bp")
 loc_anc <- left_join(loc_anc, snpfile)
-#pdf("/home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/sep_pops/100_ind/MOSAIC_PLOTS/plot_localanc.pdf")
-#plot_localanc(chrnos, g.loc, localanc)
-#dev.off()
-fwrite(loc_anc, "/home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/sep_pops/100_ind/loc_anc.csv", sep = ",", row.names = F, col.names = T, quote = F, na = "NA")
+pdf(paste(output_file_name,".pdf"))
+plot_localanc(chrnos, g.loc, localanc)
+dev.off()
+fwrite(loc_anc, paste(output_file_name, ".csv"), sep = ",", row.names = F, col.names = T, quote = F, na = "NA")
 #Okay but how do I tell which ancestry is which
