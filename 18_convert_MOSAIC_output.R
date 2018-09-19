1a3116828a71c1df8a867ac8d579ea4e3b95b881
Angela Andaleon <angie.andaleon@gmail.com>
2018-09-19 11:03:20 -0500

    Update 18_convert_MOSAIC_output.R

--- 18_convert_MOSAIC_output.R
+++ 18_convert_MOSAIC_output.R
@@ -8,7 +8,9 @@ library(tibble)
 library(reshape2)
 args <- commandArgs(trailingOnly = T)
 input_file_name <- args[1] #localanc.RData produced by MOSAIC
-output_file_name <- args[2] #output prefix
+phind_file_name <- args[2] #MOSAIC input admixed.phind
+snpfile_file_name <- args[3] #MOSAIC input snpfile
+output_file_name <- args[4] #output prefix
 
 load(input_file_name)
 df <- reshape2::melt(localanc[[1]])
@@ -20,14 +22,14 @@ g_loc$Var3 <- as.integer(g_loc$Var3)
 loc_anc <- dplyr::left_join(df, g_loc)
 loc_anc$Var3 <- NULL
 colnames(loc_anc) <- c("ancestry", "haplotype_num", "prob", "bp")
-phind <- fread("/home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/sep_pops/100_ind/HCHS_chr22.phind", header = F)
+phind <- fread(phind_file_name, header = F)
 phind <- phind %>% dplyr::select(V1)
 colnames(phind) <- "haplotype"
 phind <- tibble::rownames_to_column(phind, "haplotype_num")
 phind$haplotype_num <- as.integer(phind$haplotype_num)
 loc_anc <- left_join(loc_anc, phind)
 loc_anc$haplotype_num <- NULL
-snpfile <- fread("/home/angela/px_his_chol/ancestry_pipeline/HCHS/no_NativeAmerican-h/PrediXcan_SNPs/sep_pops/100_ind/snpfile.22")
+snpfile <- fread(snpfile_file_name)
 snpfile <- snpfile %>% dplyr::select(V3, V4)
 colnames(snpfile) <- c("cM", "bp")
 loc_anc <- left_join(loc_anc, snpfile)
