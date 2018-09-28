08c76272b6eb7eb5d4e19d7626843a1edcb0355b
Angela Andaleon <angie.andaleon@gmail.com>
2018-09-28 10:28:50 -0500

    Update 02_PrediXcan_dosages_to_GEMMA.py

--- 02_PrediXcan_dosages_to_GEMMA.py
+++ 02_PrediXcan_dosages_to_GEMMA.py
@@ -1,4 +1,6 @@
 #Takes information from PrediXcan-style dosages to make SNP annotation and BIMBAM files for GEMMA
+#cd /home/angela/px_his_chol/local_anc_GEMMA/MOSAIC_RESULTS/
+#python 02_PrediXcan_dosages_to_GEMMA.py --dosage_path /home/angela/px_his_chol/Imputation/UMich/UMich_dosages/ --local_anc_samples MOSAIC_for_GEMMA_1_ind.txt --local_anc_SNPs MOSAIC_for_GEMMA_1_snps.txt --chr 1
 import argparse
 import gzip
 import numpy as np
@@ -11,7 +13,7 @@ parser.add_argument("--dosage_path", type = str, action = "store", dest = "dosag
 #parser.add_argument("--anno_path", type = str, action = "store", dest = "anno_path", required = False, default = "anno/", help = "Name of output folder for annotations.")
 parser.add_argument("--local_anc_samples", type = str, action = "store", dest = "local_anc_samples", required = False, help = "Path to file containing samples to include (output of 19_loc_anc.py).")
 parser.add_argument("--local_anc_SNPs", type = str, action = "store", dest = "local_anc_SNPs", required = False, help = "Path to file containing SNPs to include (output of 19_loc_anc.py).")
-parser.add_argument("--chr", type = int, action = "store", dest = "chr", required = False, help = "Path to chromosome to analyze. If no input, analyzes all 22 pairs of chromosomes.")
+parser.add_argument("--chr", type = str, action = "store", dest = "chr", required = False, help = "Path to chromosome to analyze. If no input, analyzes all 22 pairs of chromosomes.")
 args = parser.parse_args()
 
 print("Reading input files.")
@@ -25,7 +27,7 @@ os.system("mkdir -p anno")
 if args.local_anc_samples is not None:
     pd.read_csv(args.local_anc_samples, sep = ":", header = None)
 if args.chr is not None:
-    chrs_to_test = range(args.chr, args.chr + 1)
+    chrs_to_test = range(int(args.chr), int(args.chr + 1))
 else:
     chrs_to_test = range(1, 23)
 if args.local_anc_SNPs is not None:
