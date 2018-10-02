494237dc19df2a17817865cb74aa1ca055195618
Angela Andaleon <angie.andaleon@gmail.com>
2018-10-02 11:18:17 -0500

    Update 23_split_haplotype_file.py

--- 23_split_haplotype_file.py
+++ 23_split_haplotype_file.py
@@ -1,4 +1,9 @@
-#haplotype files are too large and causing severe memory issues in MOSAIC, so splitting them up into 12 sections
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct 01 20:48:02 2018
+
+@author: Angela
+"""
 
 import numpy as np
 import pandas as pd
@@ -22,16 +27,13 @@ for ind_chunk in range(0, len(split_inds)):
     #subset to within chunks
     ind_chunk_include = split_inds[ind_chunk].tolist()
     ind_chunk_genofile = haplotypes[ind_chunk_include]
-    ind_chunk_genofile = pd.DataFrame(ind_chunk_genofile.astype(str).apply(''.join))
-        #must transpose b/c it doesn't like not having a delimiter
+    ind_chunk_genofile = ind_chunk_genofile.transpose()
+    ind_chunk_genofile = pd.DataFrame(ind_chunk_genofile.astype(str).apply(''.join)) #since to_csv() doesn't like running without a delimiter, we cheat a bit
+    ind_chunk_genofile_l = ind_chunk_genofile.values.tolist()
     ind_chunk_phind = phind.loc[phind[0].isin(ind_chunk_include)]
-    
-    ind_chunk_genofile.to_csv("test_" + str(ind_chunk) + "_" + str(chr) + ".phind", sep = ",", na_rep = "NA", header = False, index = False)
+
+    #write to chunks    
+    ind_chunk_genofile.to_csv("test_" + str(ind_chunk) + "genofile." + str(chr), sep = ",", na_rep = "NA", header = False, index = False)
     ind_chunk_phind.to_csv("test_" + str(ind_chunk) + "_" + str(chr) + ".phind", sep = "\t", na_rep = "NA", header = False, index = False, quoting = 3, float_format='%12f')
    
-    #why can't you be like R and accept no delimiter
-    ind_chunk_genofile = pd.read_table("test_" + str(ind_chunk) + "genofile." + str(chr), sep = ",", header = None).transpose()
-    ind_chunk_genofile = pd.DataFrame(ind_chunk_genofile.astype(str).apply(''.join))
-    ind_chunk_genofile.to_csv("test_" + str(ind_chunk) + "genofile." + str(chr), sep = "\t", na_rep = "NA", header = False, index = False)
-    
     
