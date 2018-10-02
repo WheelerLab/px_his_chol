862a6c9acca743ffb14d6675b29ff2bcc51f98cf
Angela Andaleon <angie.andaleon@gmail.com>
2018-10-02 10:12:08 -0500

    Update 22_GEMMA_wrapper_admixture_mapping.py

--- 22_GEMMA_wrapper_admixture_mapping.py
+++ 22_GEMMA_wrapper_admixture_mapping.py
@@ -10,7 +10,9 @@ pd.options.mode.chained_assignment = None
 
 parser = argparse.ArgumentParser()
 #novel-ish part of using GEMMA
+parser.add_argument("--snplist", type = str, action = "store", dest = "snplist", required = True, help = "Path to file containing a list of SNPs to be included in analysis (output of 19_loc_anc.py).")
 parser.add_argument("--snptable", type = str, action = "store", dest = "snptable", required = True, help = "Path to file containing the .csv output of 19_loc_anc.py")
+parser.add_argument("--ind_list", type = str, action = "store", dest = "ind_list", required = True, help = "Path to file containing individuals in the analysis (output of 19_loc_anc.py).")
 
 #established part of using GEMMA
 parser.add_argument("--relatedness", type = str, action = "store", dest = "relatedness", required = True, help = "Path to file containing relatedness matrix w/o IIDs for only individuals in analysis.")
@@ -51,6 +53,7 @@ anno = " -a anno/anno1.txt "
 pheno_file = "pheno_chr1.txt"
 relatedness = "relatedness_chr1.txt"
 
+SNPs = np.loadtxt("MOSAIC_for_GEMMA_1_snps.txt", dtype = 'string')#, engine='python')
 loc_anc_cov = pd.read_csv("MOSAIC_for_GEMMA_1.csv", delimiter=',', encoding="utf-8-sig")
 BIMBAM = pd.read_table(BIMBAM_file, compression='gzip', sep='\t', header = None, index_col = 0)
 
@@ -72,23 +75,24 @@ SNP_list = SNPs['rs'].tolist()
 pheno = range(1,5)
 pheno_name = ["CHOL_rank", "HDL_rank", "TRIG_rank", "LDL_rank"]
 
-#progress bar
-progress_landmarks_ind = np.linspace(0, len(inds), 21, dtype = int).tolist()
-num_ind = 0
-    
-    '''
-    Okay so for whoever bothers to read this
-    I tried making a large data frame and lists of lists with the dosages
-    However, this made the addition time for each individual raise incrementally
-    And that's not good for 12,000 people
-    So, I write each dosage as a line into a file individually so the addition time is constant
-    And then read that all back in and add the BIMBAM information
-    So I swear I tried to do it a fancier way but it's so slow with so many people
-    '''
+print("Creating ancestry-specific dosage files.")
 
 IBS_file = open("BIMBAM/IBS.txt", "a+")
 NAT_file = open("BIMBAM/NAT.txt", "a+")
 YRI_file = open("BIMBAM/YRI.txt", "a+")
+    
+progress_landmarks_ind = np.linspace(0, len(inds), 21, dtype = int).tolist()
+num_ind = 0
+    
+'''
+Okay so for whoever bothers to read this
+I tried making a large data frame and lists of lists with the dosages
+However, this made the addition time for each individual raise incrementally
+And that's not good for 12,000 people
+So, I write each dosage as a line into a file individually so the addition time is constant
+And then read that all back in and add the BIMBAM information
+So I swear I tried to do it a fancier way but it's so slow with so many people
+'''
     
 for ind in inds:
     #iterate through cols
@@ -100,7 +104,7 @@ for ind in inds:
     IBS_list = ind_df.loc['IBS'].tolist()
     NAT_list = ind_df.loc['NAT'].tolist()
     YRI_list = ind_df.loc['YRI'].tolist()
-    
+       
     IBS_file.write("\t".join(IBS_list) + "\n")
     NAT_file.write("\t".join(NAT_list) + "\n")
     YRI_file.write("\t".join(YRI_list) + "\n")
@@ -129,13 +133,13 @@ YRI_BIMBAM = pd.concat([SNPs, YRI_BIMBAM], axis=1)
 IBS_BIMBAM.to_csv("BIMBAM/IBS.txt.gz", sep = "\t", na_rep = "NA", header = False, index = False, quoting = 3, float_format='%12f', compression = "gzip")
 NAT_BIMBAM.to_csv("BIMBAM/NAT.txt.gz", sep = "\t", na_rep = "NA", header = False, index = False, quoting = 3, float_format='%12f', compression = "gzip")
 YRI_BIMBAM.to_csv("BIMBAM/YRI.txt.gz", sep = "\t", na_rep = "NA", header = False, index = False, quoting = 3, float_format='%12f', compression = "gzip")
-   
+    
 #delete intermediate files
 os.system("rm -f BIMBAM/IBS.txt")
 os.system("rm -f BIMBAM/NAT.txt")
 os.system("rm -f BIMBAM/YRI.txt")
-
-#phenotype loop
+    
+#phenotype loop    
 for pheno_num, pheno_name_rank in zip(pheno, pheno_name):
     print("Starting analyses on " + pheno_name_rank + ".")
     for pop in ['NAT', 'IBS', 'YRI']:
