97ea11d8e0eea9e4bab8560b926623fb580519c6
Angela Andaleon <angie.andaleon@gmail.com>
2018-10-30 14:58:53 -0500

    Update 24_BIMBAM_to_PX_dosages.py

--- 24_BIMBAM_to_PX_dosages.py
+++ 24_BIMBAM_to_PX_dosages.py
@@ -22,7 +22,7 @@ for anno_line, BIMBAM_line in zip(anno, BIMBAM): #go through each line in anno a
     (rs_anno, bp, chr) = a_line[0:3] #assign each part of anno line to a variable
     B_line = BIMBAM_line.strip().split(",") #split BIMBAM line into list
     (rs_BIMBAM, A1, A2) = B_line[0:3] #assign first part of BIMBAM into variable
-    dosages = B_line[4:] #assign last part of BIMBAM to a list
+    dosages = B_line[3:] #assign last part of BIMBAM to a list
     float_dosages = [float(dosage) for dosage in dosages] #convert each dosage into floats
     MAF = sum(float_dosages)/(len(dosages) * 2) #calculate minor allele frequency (I hope the math is correct)
     if MAF > 0.5: #if minor allele frequency is the other allele
