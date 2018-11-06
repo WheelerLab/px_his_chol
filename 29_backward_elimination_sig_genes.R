e8f3868d6a366387d5bc35ce6857b8c515561eaf
Angela Andaleon <angie.andaleon@gmail.com>
2018-11-06 12:03:47 -0600

    Save full and back-elim models

--- 29_backward_elimination_sig_genes.R
+++ 29_backward_elimination_sig_genes.R
@@ -2,28 +2,43 @@
 library(dplyr)
 "%&%" = function(a,b) paste(a,b,sep="")
 sig_gene_HCHS <- fread("/home/angela/px_his_chol/MESA_compare/GTEx_WB/sig_gene_HCHS.csv")
-sig_tiss_gene <- paste(sig_gene_HCHS$tissue, sig_gene_HCHS$gene, sep = "_")
 pheno <- fread('/home/angela/px_his_chol/editedPheno/11_all_lipid_rank_12236_FID_IID.txt', header = T)
 pheno <- pheno %>% select(FID, IID, CHOL_rank, HDL_rank, TRIG_rank, LDL_rank)
 phenos <- c("CHOL", "HDL", "TRIG", "LDL")
 tissues <- fread("/home/angela/px_yri_chol/PrediXcan/database_tissues.txt", header = F)
-tissues <- tissues$V1
+tissues <- tissues$V1 #get list of tissues
+back_elim <- pheno[,2] #extract only FID and IID
 
 pheno_name <- "CHOL"
 tiss_name <- "TW_Artery_Coronary_0.5.db"
 
-back_elim <- pheno[,2] #extract only FID and IID
-
-test_pheno <- subset(sig_gene_HCHS, pheno == pheno_name) #subset to just the pheno in question
-for(tiss_name in tissues){
-  tiss <- fread('/home/angela/px_his_chol/PrediXcan/' %&% tiss_name %&% '_predicted_expression.txt', header = F) #load in predicted expression file
-  tiss$V1 <- NULL #remove FID
-  tiss_header <- paste(tiss_name, "_", gsub("\\..*", "", c(tiss[1,])), sep = "") #remove all after . and add tiss name
-  tiss <- tiss[2:nrow(tiss),] #remove header line
-  colnames(tiss) <- tiss_header #assign header line
-  colnames(tiss)[1] <- "IID" #rename beginning
-  tiss_gene_in_tiss <- intersect(colnames(tiss), sig_tiss_gene) #intersection of tiss-gene combos
-  tiss_gene_in_tiss <- c("IID", tiss_gene_in_tiss) #add IID to list
-  for_back_elim <- tiss %>% select(tiss_gene_in_tiss) #select IIDs and intersection of tiss-gene combos
-  back_elim <- left_join(back_elim, for_back_elim, by = "IID") #add to list to back-elim
+for(pheno_name in phenos){
+  test_pheno <- subset(sig_gene_HCHS, pheno == pheno_name) #subset to just the pheno in question
+  sig_tiss_gene <- paste(test_pheno$tissue, test_pheno$gene, sep = "_")
+  pheno_name_rank <- paste(pheno_name, "_rank", sep = "")
+  back_elim <- pheno %>% select(IID, pheno_name_rank)
+  
+  for(tiss_name in tissues){
+    tiss <- fread('/home/angela/px_his_chol/PrediXcan/' %&% tiss_name %&% '_predicted_expression.txt', header = F, nThread = 30) #load in predicted expression file
+    tiss$V1 <- NULL #remove FID
+    tiss_header <- paste(tiss_name, "_", gsub("\\..*", "", c(tiss[1,])), sep = "") #remove all after . and add tiss name
+    tiss <- tiss[2:nrow(tiss),] #remove header line
+    colnames(tiss) <- tiss_header #assign header line
+    colnames(tiss)[1] <- "IID" #rename beginning
+    tiss_gene_in_tiss <- intersect(colnames(tiss), sig_tiss_gene) #intersection of tiss-gene combos
+    tiss_gene_in_tiss <- c("IID", tiss_gene_in_tiss) #add IID to list
+    for_back_elim <- tiss %>% select(tiss_gene_in_tiss) #select IIDs and intersection of tiss-gene combos
+    back_elim <- left_join(back_elim, for_back_elim, by = "IID") #add to list to back-elim
+  }
+  
+  back_elim$IID <- NULL #the IID column has served its purpose
+  back_elim <- back_elim[complete.cases(back_elim),]
+  predictor_genes <- colnames(back_elim)[2:length(colnames(back_elim))] #https://stackoverflow.com/questions/5251507/how-to-succinctly-write-a-formula-with-many-variables-from-a-data-frame
+  fmla <- as.formula(paste(pheno_name_rank,  " ~ ", paste(predictor_genes, collapse= "+")))
+  all_tiss_gene <- lm(fmla, data = back_elim) 
+  saveRDS(all_tiss_gene, file = pheno_name %&% "_all_tiss_gene.rds")
+  back_elim_complete <- step(all_tiss_gene, direction = "backward", trace = FALSE) #perform backward analysis on full model
+  saveRDS(back_elim_complete, file = pheno_name %&% "_back_elim.rds")
 }
+
+
