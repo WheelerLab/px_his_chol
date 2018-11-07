17c2a34c60de004234ea29146393fd37b3bfe1cf
Angela Andaleon <angie.andaleon@gmail.com>
2018-11-07 10:06:34 -0600

    triple loop b/c idk what else to do

--- 30_make_coloc_input.R
+++ 30_make_coloc_input.R
@@ -4,29 +4,39 @@ library(R.utils)
 
 "%&%" = function(a,b) paste(a,b,sep="")
 phenos <- c("CHOL_rank", "HDL_rank", "TRIG_rank", "LDL_rank")
+chrs <- c(1:22)
+pops <- c("AFA", "CAU", "HIS") #do combined pops later
+pops_sample_size <- c(233, 352, 578)
 
-frq <- fread("/home/angela/px_his_chol/MESA_compare/HIS_rm_missnps.frq")
-frq <- frq %>% select(SNP, MAF)
-meqtl <- fread("/home/lauren/files_for_revisions_plosgen/meqtl_results/MESA/HIS_Nk_10_PFs_chr20pcs_3.meqtl.cis.2018-04-04.txt.gz")
-meqtl$se <- meqtl$beta / meqtl$statistic
-meqtl$n_samples <- 352
-meQTL_for_COLOC <- left_join(meqtl, frq, by = c("snps" = "SNP"))
-meQTL_for_COLOC <- meQTL_for_COLOC %>% select(gene, snps, MAF, pvalue, beta, se)
-colnames(meQTL_for_COLOC) <- c("gene_id", "variant_id", "maf", "pval_nominal", "slope", "slope_se")
-meQTL_for_COLOC <- meQTL_for_COLOC[complete.cases(meQTL_for_COLOC),]
+for(pop in length(pops)){ #read in pop's .frq file for MAF
+  frq <- fread("/home/angela/px_his_chol/MESA_compare/" %&% pops[pop] %&% "_rm_missnps.frq")
+  frq <- frq %>% select(SNP, MAF)
 
-GEMMA_result <- fread("/home/angela/px_his_chol/GEMMA/output/CHOL_rank_sig_snps_p-0.txt", header = T)
-GEMMA_result$chr_pos <- paste(gsub("chr", "", GEMMA_result$chr), GEMMA_result$ps, sep = ":")
-GEMMA_for_COLOC <- GEMMA_result %>% select(rs, beta, se, af)
-GEMMA_for_COLOC$sample_size <- 11103
-colnames(GEMMA_for_COLOC) <- c("panel_variant_id", "effect_size", "standard_error", "frequency", "sample_size")
+  for(pheno in phenos){ #read in GEMMA output file
+    GEMMA_result <- fread("/home/angela/px_his_chol/GEMMA/output/" %&% pheno %&% "_sig_snps_p-0.txt", header = T)
+    GEMMA_result$chr_pos <- paste(gsub("chr", "", GEMMA_result$chr), GEMMA_result$ps, sep = ":")
+    GEMMA_for_COLOC <- GEMMA_result %>% select(rs, beta, se, af) #subset to COLOC input
+    GEMMA_for_COLOC$sample_size <- 11103
+    colnames(GEMMA_for_COLOC) <- c("panel_variant_id", "effect_size", "standard_error", "frequency", "sample_size")
+    GEMMA_for_COLOC <- GEMMA_for_COLOC[complete.cases(GEMMA_for_COLOC),] #COLOC does not like missing values
+    
+    for(chr in chrs){ #yes triple loops are ratchet
+      meqtl <- fread("/home/lauren/files_for_revisions_plosgen/meqtl_results/MESA/" %&% pops[pop] %&% "_Nk_10_PFs_chr" %&% chr %&% "pcs_3.meqtl.cis.2018-04-04.txt.gz") #read in matrix eQTL results
+      meqtl$se <- meqtl$beta / meqtl$statistic #make your own standard error since it's not in the meQTL output
+      meqtl$n_samples <- pops_sample_size[pop]
+      meQTL_for_COLOC <- left_join(meqtl, frq, by = c("snps" = "SNP")) #add freq to COLOC input
+      meQTL_for_COLOC <- meQTL_for_COLOC %>% select(gene, snps, MAF, pvalue, beta, se) #subset to COLOC input
+      colnames(meQTL_for_COLOC) <- c("gene_id", "variant_id", "maf", "pval_nominal", "slope", "slope_se")
+      meQTL_for_COLOC <- meQTL_for_COLOC[complete.cases(meQTL_for_COLOC),]
 
-snps_in_both <- intersect(GEMMA_for_COLOC$panel_variant_id, meQTL_for_COLOC$variant_id)
-GEMMA_for_COLOC <- subset(GEMMA_for_COLOC, panel_variant_id %in% snps_in_both)
-meQTL_for_COLOC <- subset(meQTL_for_COLOC, variant_id %in% snps_in_both)
+      snps_in_both <- intersect(GEMMA_for_COLOC$panel_variant_id, meQTL_for_COLOC$variant_id) #is there a better way to do this? Probably. Do I feel like figuring it out? Nah.
+      GEMMA_for_COLOC_chr <- subset(GEMMA_for_COLOC, panel_variant_id %in% snps_in_both)
+      meQTL_for_COLOC_chr <- subset(meQTL_for_COLOC, variant_id %in% snps_in_both)
 
-fwrite(meQTL_for_COLOC, "/home/angela/px_his_chol/COLOC/HIS_for_COLOC_chr20.txt", quote = F, sep = "\t", na = "NA", row.names = F, col.names = T)
-gzip("/home/angela/px_his_chol/COLOC/HIS_for_COLOC_chr20.txt", destname = "/home/angela/px_his_chol/COLOC/HIS_for_COLOC_chr10.txt.gz")
-fwrite(GEMMA_for_COLOC, "/home/angela/px_his_chol/COLOC/CHOL_rank_chr20_for_COLOC.txt", row.names = F, col.names = T, sep = "\t", quote = F, na = "NA")
-gzip("/home/angela/px_his_chol/COLOC/CHOL_rank_chr20_for_COLOC.txt", destname = "/home/angela/px_his_chol/COLOC/CHOL_rank_chr20_for_COLOC.txt.gz")
-
+      fwrite(meQTL_for_COLOC_chr, "/home/angela/px_his_chol/COLOC/" %&% pops[pop] %&% "_for_COLOC_chr" %&% chr %&% ".txt", quote = F, sep = "\t", na = "NA", row.names = F, col.names = T)
+      gzip("/home/angela/px_his_chol/COLOC/" %&% pops[pop] %&% "_for_COLOC_chr" %&% chr %&% ".txt", destname = "/home/angela/px_his_chol/COLOC/" %&% pops[pop] %&% "_for_COLOC_chr" %&% chr %&% ".txt.gz") #script may only take .gz values so can't hurt to be too careful
+      fwrite(GEMMA_for_COLOC_chr, "/home/angela/px_his_chol/COLOC/" %&% pheno %&% "_chr" %&% chr %&% "_for_COLOC.txt", row.names = F, col.names = T, sep = "\t", quote = F, na = "NA")
+      gzip("/home/angela/px_his_chol/COLOC/" %&% pheno %&% "_chr" %&% chr %&% "_for_COLOC.txt", "/home/angela/px_his_chol/COLOC/" %&% pheno %&% "_chr" %&% chr %&% "_for_COLOC.txt.gz")
+    }
+  }
+}
