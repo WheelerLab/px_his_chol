63a8ebde5dd5b5db87d575d19e3f1e539ba4a6e7
Angela Andaleon <angie.andaleon@gmail.com>
2018-11-12 10:07:58 -0600

    combine chrs instead of having a bunch of different ones

--- 30a_make_coloc_input_MESA.R
+++ 30a_make_coloc_input_MESA.R
@@ -1,4 +1,3 @@
-#make coloc input from GEMMA results and matrix eQTL data from MESA models 
 library(data.table)
 library(dplyr)
 library(R.utils)
@@ -8,20 +7,22 @@ phenos <- c("CHOL_rank", "HDL_rank", "TRIG_rank", "LDL_rank")
 chrs <- c(1:22)
 pops <- c("AFA", "CAU", "HIS", "AFHI", "ALL") #do combined pops later
 pops_sample_size <- c(233, 578, 352, 585, 1163) #R doesn't have dicts so we're doing it a slgihtly more ratchet way
-sig_gene_SNPs <- fread("/home/angela/px_his_chol/MESA_compare/sig_gene_weights/MESA_sig_gene_SNPs.txt", header = F) #so we don't run all the SNPs b/c it takes forever
+sig_gene_SNPs <- fread("/home/angela/px_his_chol/COLOC/COLOC_input/sig_gene_SNPs_84.txt", header = F) #so we don't run all the SNPs b/c it takes forever
 sig_gene_SNPs <- sig_gene_SNPs$V1
 
 for(pop in 1:length(pops)){ #read in pop's .frq file for MAF
   frq <- fread("/home/angela/px_his_chol/MESA_compare/" %&% pops[pop] %&% ".frq")
-  frq <- frq %>% select(SNP, MAF)
+  frq <- frq %>% dplyr::select(SNP, MAF)
 
   for(pheno in phenos){ #read in GEMMA output file
     GEMMA_result <- fread("/home/angela/px_his_chol/GEMMA/output/" %&% pheno %&% "_sig_snps_p-0.txt", header = T)
     GEMMA_result$chr_pos <- paste(gsub("chr", "", GEMMA_result$chr), GEMMA_result$ps, sep = ":")
-    GEMMA_for_COLOC <- GEMMA_result %>% select(rs, beta, se, af) #subset to COLOC input
+    GEMMA_for_COLOC <- GEMMA_result %>% dplyr::select(rs, beta, se, af) #subset to COLOC input
     GEMMA_for_COLOC$sample_size <- 11103
     colnames(GEMMA_for_COLOC) <- c("panel_variant_id", "effect_size", "standard_error", "frequency", "sample_size")
     GEMMA_for_COLOC <- GEMMA_for_COLOC[complete.cases(GEMMA_for_COLOC),] #COLOC does not like missing values
+    GWAS_write <- data.frame(panel_variant_id = character(), effect_size = numeric(), standard_error = numeric(), frequency = numeric(), sample_size = numeric(), stringsAsFactors = F) 
+    eQTL_write <- data.frame(gene_id = character(), variant_id = character(), maf = numeric(), pval_nominal = numeric(), slope = numeric(), slope_se = numeric(), stringsAsFactors = F) 
     
     for(chr in chrs){ #yes triple loops are ratchet
       system("zcat -f /home/lauren/files_for_revisions_plosgen/meqtl_results/MESA/" %&% pops[pop] %&% "_Nk_10_PFs_chr" %&% chr %&% "pcs_3.meqtl.cis.* > /home/angela/px_his_chol/COLOC/COLOC_input/meQTL_input.txt") #fread doesn't seem to like wildcards so we're gonna do this the ugly way
@@ -29,7 +30,7 @@ for(pop in 1:length(pops)){ #read in pop's .frq file for MAF
       meqtl$se <- meqtl$beta / meqtl$statistic #make your own standard error since it's not in the meQTL output
       meqtl$n_samples <- pops_sample_size[pop]
       meQTL_for_COLOC <- left_join(meqtl, frq, by = c("snps" = "SNP")) #add freq to COLOC input
-      meQTL_for_COLOC <- meQTL_for_COLOC %>% select(gene, snps, MAF, pvalue, beta, se) #subset to COLOC input
+      meQTL_for_COLOC <- meQTL_for_COLOC %>% dplyr::select(gene, snps, MAF, pvalue, beta, se) #subset to COLOC input
       colnames(meQTL_for_COLOC) <- c("gene_id", "variant_id", "maf", "pval_nominal", "slope", "slope_se")
       meQTL_for_COLOC <- meQTL_for_COLOC[complete.cases(meQTL_for_COLOC),]
 
@@ -38,11 +39,14 @@ for(pop in 1:length(pops)){ #read in pop's .frq file for MAF
       GEMMA_for_COLOC_chr <- subset(GEMMA_for_COLOC, panel_variant_id %in% snps_in_all)
       meQTL_for_COLOC_chr <- subset(meQTL_for_COLOC, variant_id %in% snps_in_all)
 
-      fwrite(meQTL_for_COLOC_chr, "/home/angela/px_his_chol/COLOC/COLOC_input/eQTL_" %&% pops[pop] %&% "_chr" %&% chr %&% "_" %&% pheno %&% ".txt", quote = F, sep = "\t", na = "NA", row.names = F, col.names = T)
-      gzip("/home/angela/px_his_chol/COLOC/COLOC_input/eQTL_" %&% pops[pop] %&% "_chr" %&% chr %&% "_" %&% pheno %&% ".txt", destname = "/home/angela/px_his_chol/COLOC/COLOC_input/eQTL_" %&% pops[pop] %&% "_chr" %&% chr %&% "_" %&% pheno %&% ".txt.gz") #script may only take .gz values so can't hurt to be too careful
-      fwrite(GEMMA_for_COLOC_chr, "/home/angela/px_his_chol/COLOC/COLOC_input/GWAS_" %&% pops[pop] %&% "_chr" %&% chr %&% "_" %&% pheno %&% ".txt", row.names = F, col.names = T, sep = "\t", quote = F, na = "NA")
-      gzip("/home/angela/px_his_chol/COLOC/COLOC_input/GWAS_" %&% pops[pop] %&% "_chr" %&% chr %&% "_" %&% pheno %&% ".txt", "/home/angela/px_his_chol/COLOC/COLOC_input/GWAS_" %&% pops[pop] %&% "_chr" %&% chr %&% "_" %&% pheno %&% ".txt.gz")
-      print("Completed with " %&% pops[pop] %&% ", chr" %&% chr %&% ", for " %&% pheno %&% ".")
+      GWAS_write <- rbind(GWAS_write, GEMMA_for_COLOC_chr)
+      eQTL_write <- rbind(eQTL_write, meQTL_for_COLOC_chr)
     }
+    
+    fwrite(eQTL_write, "/home/angela/px_his_chol/COLOC/COLOC_input/eQTL_" %&% pops[pop] %&% "_" %&% pheno %&% ".txt", quote = F, sep = "\t", na = "NA", row.names = F, col.names = T)
+    gzip("/home/angela/px_his_chol/COLOC/COLOC_input/eQTL_" %&% pops[pop] %&% "_" %&% pheno %&% ".txt", destname = "/home/angela/px_his_chol/COLOC/COLOC_input/eQTL_" %&% pops[pop] %&% "_" %&% pheno %&% ".txt.gz") #script may only take .gz values so can't hurt to be too careful
+    fwrite(GWAS_write, "/home/angela/px_his_chol/COLOC/COLOC_input/GWAS_" %&% pops[pop] %&% "_" %&% pheno %&% ".txt", row.names = F, col.names = T, sep = "\t", quote = F, na = "NA")
+    gzip("/home/angela/px_his_chol/COLOC/COLOC_input/GWAS_" %&% pops[pop] %&% "_" %&% pheno %&% ".txt", "/home/angela/px_his_chol/COLOC/COLOC_input/GWAS_" %&% pops[pop] %&% "_" %&% pheno %&% ".txt.gz")
+    print("Completed with " %&% pops[pop] %&% ", for " %&% pheno %&% ".")
   }
 }
